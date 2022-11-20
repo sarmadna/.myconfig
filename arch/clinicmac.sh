d3245594d6258c141126a3c2d6bee9cf04402a69
sarmadna <sarmad_n_a@hotmail.com>
2022-11-20 21:53:35 +0300

    update

--- arch/clinicmac.sh
+++ arch/clinicmac.sh
@@ -12,33 +12,35 @@ sed -i '171s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-echo "arch" >> /etc/hostname
-echo "127.0.0.1		arch" >> /etc/hosts
-echo "::1		arch" >> /etc/hosts
-echo "127.0.1.1		arch.localdomain	arch" >> /etc/hosts
+echo "archlinux" >> /etc/hostname
+echo "127.0.0.1		archlinux" >> /etc/hosts
+echo "::1		archlinux" >> /etc/hosts
+echo "127.0.1.1		archlinux.localdomain	archlinux" >> /etc/hosts
 
 sed -i '37s/.//' /etc/pacman.conf
 pacman -S \
 	sudo \
 	grub \
-	dialog \
-	pipewire \
-	firewalld \
 	efibootmgr \
 	base-devel \
 	linux-headers \
-	pipewire-alsa \
-	pipewire-jack \
-	pipewire-pulse \
+	acpi \
+	acpid \
+	acpi_call \
+	dialog \
+	firewalld \
+	broadcom-wl \
 	wpa_supplicant \
 	networkmanager \
 	network-manager-applet
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable firewalld
+systemctl enable acpid
+systemctl enable tlp
 
 useradd -m -c "Sarmad" sarmad
 echo sarmad:1 | chpasswd
